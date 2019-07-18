24f7d9656f6d74fc2eb82090903f1dcc0b5b16d3
Carlos Del Real <carloshdelreal@gmail.com>
2019-07-18 17:56:31 -0500

    Per capita GDP, South america in millions

--- 1_SELECT_world.sql
+++ 1_SELECT_world.sql
@@ -6,3 +6,15 @@ SELECT name, continent, population FROM world
 
 SELECT name FROM world
 WHERE population > 200000000
+
+-- 3. Per capita GDP
+
+SELECT name, gdp/population 
+FROM world
+WHERE population > 200000000
+
+-- 4. South America In millions
+
+SELECT name, population/1000000
+FROM world
+WHERE continent = 'South America'
